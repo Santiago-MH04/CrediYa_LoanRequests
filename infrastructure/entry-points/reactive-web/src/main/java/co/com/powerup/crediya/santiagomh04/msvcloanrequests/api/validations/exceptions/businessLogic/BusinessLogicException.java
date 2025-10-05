1986888b8a364a92edc9996b4c305b7e7aab336a
Santiago <santimh04@gmail.com>
2025-10-04 19:21:16 -0500

    feat: validations running and WebClient processing loan requests creation

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/crediya/santiagomh04/msvcloanrequests/api/validations/exceptions/businessLogic/BusinessLogicException.java
@@ -0,0 +1,7 @@
+package co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.validations.exceptions.businessLogic;
+
+public class BusinessLogicException extends RuntimeException {
+    public BusinessLogicException(String message) {
+        super(message);
+    }
+}
