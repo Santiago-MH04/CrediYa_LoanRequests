085645c16cc815f6d9b5d84d9424a62275d17ec3
Santiago <santimh04@gmail.com>
2025-10-06 11:36:07 -0500

    feat: logging centralised, including all scenarios, implemented and working

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/crediya/santiagomh04/msvcloanrequests/api/handlers/exceptionHandler/GlobalExceptionHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/crediya/santiagomh04/msvcloanrequests/api/handlers/exceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.handlers.exceptionHandler;
 
 import co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.dto.ErrorResponseDTO;
+import co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.handlers.loggingHelpers.HandlerLoggingSupport;
 import co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.validations.exceptions.businessLogic.BusinessLogicException;
 import co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.validations.exceptions.gateway.BadGatewayException;
 import co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.validations.exceptions.requestBody.RequestBodyException;
@@ -30,10 +31,13 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
         // Add more exceptions here and their HttpStatus codes when necessary
     );
 
-    public GlobalExceptionHandler(ErrorAttributes errorAttributes, ApplicationContext applicationContext, ServerCodecConfigurer serverCodecConfigurer) {
+    private final HandlerLoggingSupport loggingSupport;
+
+    public GlobalExceptionHandler(ErrorAttributes errorAttributes, ApplicationContext applicationContext, ServerCodecConfigurer serverCodecConfigurer, HandlerLoggingSupport loggingSupport) {
         super(errorAttributes, new WebProperties.Resources(), applicationContext);
         this.setMessageWriters(serverCodecConfigurer.getWriters());
         this.setMessageReaders(serverCodecConfigurer.getReaders());
+        this.loggingSupport = loggingSupport;
     }
 
     @Override
@@ -43,18 +47,13 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
 
     private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
         Throwable error = getError(request);
-        HttpStatus status = this.determineHttpStatus(error);
+        HttpStatus status = determineHttpStatus(error);
 
-        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
-            LocalDateTime.now(),
-            request.path(),
-            error.getMessage(),
-            status.toString()
+        return this.loggingSupport.handleError(
+            request,
+            error,
+            status
         );
-
-        return ServerResponse.status(status.value())
-            .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(errorResponse);
     }
 
     private HttpStatus determineHttpStatus(Throwable error) {
---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/crediya/santiagomh04/msvcloanrequests/api/handlers/loggingHelpers/loggingEnums/LogLevel.java
@@ -0,0 +1,10 @@
+package co.com.powerup.crediya.santiagomh04.msvcloanrequests.api.handlers.loggingHelpers.loggingEnums;
+
+public enum LogLevel {
+    INFO,
+    ERROR,
+    DEBUG,
+    WARN,
+    TRACE
+}
+
